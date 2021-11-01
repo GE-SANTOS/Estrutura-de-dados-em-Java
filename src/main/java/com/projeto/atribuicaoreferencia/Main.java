c831863f57ea1fa794d02522f6edd3c6dc2bf968
Geovane Santos <geovane_n.s@outlook.com>
2021-11-01 17:37:47 -0300

    Atribuição e Referência em Java

--- src/main/java/com/projeto/atribuicaoreferencia/Main.java
+++ src/main/java/com/projeto/atribuicaoreferencia/Main.java
@@ -3,6 +3,27 @@ package com.projeto.atribuicaoreferencia;
 public class Main {
 
     public static void main(String[] args) {
-        
+        //Atribuição de tipos primitivos
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("intA=" + intA + " intB=" + intB);
+
+        intA = 2;
+
+        System.out.println("intA=" + intA + " intB=" + intB);
+        //Atribuição de objetos
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB =objA;
+
+        System.out.println("objA=" + objA + " objB=" + objB);
+
+        objA.setNum(2);
+
+        System.out.println("objA=" + objA + " objB=" + objB);
+
+
+
+
     }
 }
